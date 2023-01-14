ba6d8eac821f05664768a5f140486dad4eff2ab0
surshreya <surshreya.0727@gmail.com>
2023-01-14 23:50:48 +0530

    API - reading  endpoints

--- src/index.js
+++ src/index.js
@@ -14,7 +14,11 @@ const app = express();
 // Setting middlewares
 app.use(express.json());
 
-// Creating routes
+// Setting up routes
+
+/**
+ * Creation endpoints
+ */
 app.post("/users", (req, res) => {
   const user = new User(req.body);
 
@@ -24,7 +28,7 @@ app.post("/users", (req, res) => {
       res.status(201).send(user);
     })
     .catch((error) => {
-      res.status(400).send(error);
+      res.status(500).send(err);
     });
 });
 
@@ -37,7 +41,58 @@ app.post("/tasks", (req, res) => {
       res.status(201).send(task);
     })
     .catch((error) => {
-      res.status(400).send(error);
+      res.status(500).send(err);
+    });
+});
+
+/**
+ * Reading endpoints
+ */
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.status(200).send(users);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).send();
+      }
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.status(200).send(tasks);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        res.status(404).send();
+      }
+      res.status(200).send(task);
+    })
+    .catch((err) => {
+      res.status(500).send(err);
     });
 });
 
