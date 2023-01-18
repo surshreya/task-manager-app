af5ea5b89ee10d5e37dccb8f3e9c61b6a536ed46
surshreya <surshreya.0727@gmail.com>
2023-01-18 10:18:21 +0530

    mail bug fix

--- emails/mail.js
+++ emails/mail.js
@@ -1,7 +1,7 @@
 const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-const sendWelcomeMail = (email, name) => {
+const sendWelcomeMail = (name, email) => {
   sgMail.send({
     to: email,
     from: "shreya.sur1994@gmail.com",
@@ -10,7 +10,7 @@ const sendWelcomeMail = (email, name) => {
   });
 };
 
-const sendCancelMail = (email, name) => {
+const sendCancelMail = (name, email) => {
   sgMail.send({
     to: email,
     from: "shreya.sur1994@gmail.com",
